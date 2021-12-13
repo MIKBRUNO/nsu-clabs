93e39f32eff9e8c116615d6e8e8a9ed5f9f574d9
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2021-12-13 19:32:01 +0700

    QuickSort INIT

---
+++ lab3-1/src/QuickSort.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+
+void quickSort(int* arr, size_t size) {
+	size_t l, r;
+	int m;
+	while (1 < size) {
+		l = 0u;
+		r = size - 1u;
+		m = (arr[r] + arr[l]) / 2;
+		while (arr[l] < m)
+			++l;
+		while (arr[r] > m)
+			--r;
+		while (l < r) {
+			if (arr[l] != arr[r]) {
+				arr[l] += arr[r];
+				arr[r] = arr[l] - arr[r];
+				arr[l] -= arr[r];
+			}
+			--r;
+			++l;
+			while (arr[l] < m)
+				++l;
+			while (arr[r] > m)
+				--r;
+		}
+		if (0 == size - r - 1)
+			return;
+		if (r + 1 < size - r - 1) {
+			quickSort(arr, r + 1);
+			arr += r + 1;
+			size -= r + 1;
+		}
+		else {
+			quickSort(arr + r + 1, size - r - 1);
+			size = r + 1;
+		}
+	}
+}
--- lab3-1/src/main.c
+++ lab3-1/src/main.c
@@ -1,5 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include "QuickSort.h"
 
 int main(void) {
-    return EXIT_SUCCESS;
+	size_t n;
+	if (!scanf("%d", &n)) {
+		exit(EXIT_SUCCESS);
+	}
+
+	int* arr;
+	if (NULL == (arr = malloc(sizeof(int) * n)))
+		exit(EXIT_SUCCESS);
+	for (size_t i = 0; i < n; ++i) {
+		if (!scanf("%d", arr + i)) {
+			exit(EXIT_SUCCESS);
+		}
+	}
+
+	quickSort(arr, n);
+	for (size_t i = 0; i < n; ++i) {
+		printf("%d ", arr[i]);
+	}
+
+	return EXIT_SUCCESS;
 }
