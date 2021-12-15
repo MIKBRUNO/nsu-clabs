c20eb1ec2d20ad446b93ad8a4d89011d8defdd58
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2021-12-15 09:35:36 +0700

    remade loop

--- lab3-1/src/QuickSort.c
+++ lab3-1/src/QuickSort.c
@@ -5,23 +5,23 @@ void quickSort(int* arr, size_t size) {
 		size_t l = 0u,
 			r = size - 1u;
 		int m = (arr[r] + arr[l]) / 2;
-		while (arr[l] < m)
-			++l;
-		while (arr[r] > m)
-			--r;
-		while (l < r) {
-			if (arr[l] != arr[r]) {
-				arr[l] += arr[r];
-				arr[r] = arr[l] - arr[r];
-				arr[l] -= arr[r];
-			}
-			--r;
-			++l;
+		do {
 			while (arr[l] < m)
 				++l;
 			while (arr[r] > m)
 				--r;
-		}
+			if (l < r) {
+				if (arr[l] != arr[r]) {
+					int t = arr[l];
+					arr[l] = arr[r];
+					arr[r] = t;
+				}
+				--r;
+				++l;
+			}
+			else
+				break;
+		} while (1);
 		if (0 == size - r - 1)
 			return;
 		if (r + 1 < size - r - 1) {
