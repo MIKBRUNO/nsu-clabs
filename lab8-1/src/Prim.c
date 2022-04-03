a193fffef290acbdd9aaccb385b26891450d7b71
m.dyachenko2 <m.dyachenko2@g.nsu.ru>
2022-04-03 20:38:53 +0700

    fixed prevous fix

--- lab8-1/src/Prim.c
+++ lab8-1/src/Prim.c
@@ -73,7 +73,7 @@ int minSpanningTree(int* adjm, unsigned short* res, size_t vcount) {
 					outEdges[v]->v[1] = v;
 					++oecount;
 				}
-				if ((int)outEdges[v]->weight <= adjm[minV * vcount + v])
+				if (outEdges[v]->weight <= (unsigned int)adjm[minV * vcount + v])
 					continue;
 
 				outEdges[v]->weight = adjm[minV * vcount + v];
